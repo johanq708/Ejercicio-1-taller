d7fd87648c0325802571941d997bd8e453b12e74
Estudiante <Estudiante@DESKTOP-KS3QO24>
2017-04-03 15:51:25 -0500

    Ejercicio 1 taller

---
+++ Rueda.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Rueda {
+    private int tamaño;
+    private String marca;
+
+    public Rueda(int tamaño, String marca) {
+        this.tamaño = tamaño;
+        this.marca = marca;
+    }
+
+    public int getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(int tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public String toString() {
+        return " de tamaño" + tamaño + " y de marca: " + marca;
+    }
+    
+}
